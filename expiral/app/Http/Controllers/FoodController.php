f0c5d8a087239b0397004798cef4a583d0a23eb3
Evan Horsley <evy0311@gmail.com>
2020-11-14 12:04:26 -0500

    Adding in food controller

--- expiral/app/Http/Controllers/FoodController.php
+++ expiral/app/Http/Controllers/FoodController.php
@@ -10,28 +10,50 @@ class FoodController extends Controller
 {
 
     public function addFood(Request $request) {
+        $id = Auth::id();
 
+        $food = new Food;
+
+        $food->food_name = $request->food_name;
+        $food->food_upc = $request->food_upc;
+        $food->expiration_date = $request->expiration_date;
+        $food->user_id = $id;
+        $food->is_expired = 0;
+
+        $food->save();
+
+        return response()->json(['message' => 'Food added successfully!'], 200);
     }
 
     public function removeFood(Request $request) {
+        $id = Auth::id();
 
+
+        return response()->json(['message' => 'Food removed successfully!'], 200);
     }
 
     public function updateFood(Request $request) {
+        $id = Auth::id();
 
+
+        return response()->json(['message' => 'Food updated successfully!'], 200);
     }
 
     public function expireFood(Request $request) {
+        $id = Auth::id();
 
+
+        return response()->json(['message' => 'Food expired successfully!'], 200);
     }
 
     public function getFood(Request $request) {
         $id = Auth::id();
-        echo $id;
 
         $food = Food::where('user_id', $id)
                ->orderBy('expiration_date', 'desc')
                ->take(10)
                ->get();
+
+        return response()->json($food, 200);
     }
 }
