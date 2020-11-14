beef3c4ad61a4de0fc643b2d73d594f0665c7e1f
Evan Horsley <evy0311@gmail.com>
2020-11-14 02:29:50 -0500

    Fixing route

--- expiral/app/Http/Controllers/AuthController.php
+++ expiral/app/Http/Controllers/AuthController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 
+
 class AuthController extends Controller
 {
     public function register (Request $request) {
--- expiral/routes/api.php
+++ expiral/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+//Controllers
+use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -23,8 +27,8 @@ Route::group(['middleware' => ['cors', 'json.response']], function () {
     // ...
 
     // public routes
-    Route::post('/login', 'AuthController@login')->name('login.api');
-    Route::post('/register','AuthController@register')->name('register.api');
+    Route::post('/login', 'App\Http\Controllers\AuthController@login')->name('login.api');
+    Route::post('/register','App\Http\Controllers\AuthController@register')->name('register.api');
     
 
     // ...
@@ -34,5 +38,5 @@ Route::group(['middleware' => ['cors', 'json.response']], function () {
 Route::middleware('auth:api')->group(function () {
     // our routes to be protected will go in here
 
-    Route::post('/logout', 'AuthController@logout')->name('logout.api');
+    Route::post('/logout', 'App\Http\Controllers\AuthController@logout')->name('logout.api');
 });
