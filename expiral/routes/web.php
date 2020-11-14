62e9d3e6d6700f7725061f91d9806590caea8c50
Evan Horsley <evy0311@gmail.com>
2020-11-14 13:56:59 -0500

    Added in dashboard route

--- expiral/routes/web.php
+++ expiral/routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/dashboard', function () {
+    return "Dashboard route working";
+    // return view('dashboard');
+});
