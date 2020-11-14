2342ddc1d65c4c8901351c3400c392daa26846e0
Evan Horsley <evy0311@gmail.com>
2020-11-14 18:46:41 -0500

    Added in JS

--- expiral/public/js/api.js
+++ expiral/public/js/api.js
@@ -5,19 +5,19 @@
 const base_url = 'http://ec2-18-188-72-25.us-east-2.compute.amazonaws.com';
 
 function login(email, password) {
-    console.log(email);
-    console.log(password);
-    const userAction = async () => {
-        const response = await fetch(base_url + '/api/login', {
-            method: 'POST',
-            body: JSON.stringify({ email: email, password: password, password_confirmation: password }), // string or object
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        const myJson = await response.json(); //extract JSON from the http response
-        // do something with myJson
-        console.log(response);
-        console.log(myJson);
-    }
+    fetch(base_url + '/api/login', {
+        method: 'post',
+        body: JSON.stringify({ email: email, password: password, password_confirmation: password }), // string or object
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(function (response) {
+        console.log(response.json());
+        return response.json();
+    });
+
+    // const myJson = await response.json(); //extract JSON from the http response
+    // // do something with myJson
+    // console.log(response);
+    // console.log(myJson);
 }
