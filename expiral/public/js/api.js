ce0ef5f20db17b181a1e4e7d673b03a437f402e5
Evan Horsley <evy0311@gmail.com>
2020-11-14 19:04:00 -0500

    Added in JS and editing form

--- expiral/public/js/api.js
+++ expiral/public/js/api.js
@@ -7,8 +7,7 @@ window.onload = function () {
     document.getElementById("sign-in-button").addEventListener("click", function () {
         var email = document.getElementById('userEmail').value;
         var password = document.getElementById('userPassword').value;
-        console.log(email);
-        console.log(password);
+
         login(email, password);
     });
 }
@@ -17,10 +16,11 @@ const base_url = 'http://ec2-18-188-72-25.us-east-2.compute.amazonaws.com';
 
 function login(email, password) {
     fetch(base_url + '/api/login', {
-        method: 'post',
-        body: JSON.stringify({ email: email, password: password, password_confirmation: password }), // string or object
+        method: 'POST',
+        body: 'email=' + email + '&password=' + password + '&password_confirmation=' + password, // string or object
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/x-www-form-url-encoded',
+            'Accept': 'application/json'
         }
     }).then(function (response) {
         return response.json();
